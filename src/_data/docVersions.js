aeabbdcde17438d2cc5380c459dcaf45b9e459a8
objz <me@objz.dev>
2026-03-01 00:39:46 +0100

    chore: update version registry to 3.2.0 and refresh site metadata

--- src/_data/docVersions.js
+++ src/_data/docVersions.js
@@ -1,20 +1,15 @@
 module.exports = {
-  latest: "3.1.0",
+  latest: "3.2.0",
   versions: [
     {
-      id: "3.1.0",
-      label: "3.1.0",
-      pathPrefix: "/docs/3.1.0",
+      id: "3.2.0",
+      label: "3.2.0",
+      pathPrefix: "/docs/3.2.0",
     },
     {
       id: "3.0.0",
       label: "3.0.0",
       pathPrefix: "/docs/3.0.0",
-    },
-    {
-      id: "2.x",
-      label: "v2.x (legacy)",
-      externalUrl: "https://objz.github.io/CommandBridge/getting-started.html",
     },
   ],
 };
