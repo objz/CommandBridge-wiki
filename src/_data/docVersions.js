39fca3af739b5ce23cd20790358a5a747b8c0a7c
objz <me@objz.dev>
2026-05-25 17:05:23 +0200

    fixed version list

--- src/_data/docVersions.js
+++ src/_data/docVersions.js
@@ -2,6 +2,16 @@ module.exports = {
   latest: "3.3.3",
   versions: [
     {
+      id: "3.3.5",
+      label: "3.3.5",
+      pathPrefix: "/docs/3.3.5",
+    },
+    {
+      id: "3.3.4",
+      label: "3.3.4",
+      pathPrefix: "/docs/3.3.4",
+    },
+    {
       id: "3.3.3",
       label: "3.3.3",
       pathPrefix: "/docs/3.3.3",
